00dfc66896553e8ca1381a6d257929c945efaa32
Oleh Prypin <oleh@pryp.in>
2021-01-01 12:23:27 +0100

    hlmd

--- normalize_file.py
+++ normalize_file.py
@@ -13,5 +13,6 @@ html = soup.prettify()
 html = re.sub(r'\b(0x)[a-f0-9]+\b', r'\1...', html)
 html = re.sub(r'^(Build Date UTC ?:).+', r'\1...', html, flags=re.MULTILINE)
 html = re.sub(r'\b[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\b', r'...', html)
+html = html.replace('<div class="codehilite">', '<div class="highlight">')
 
 path.write_text(html)
